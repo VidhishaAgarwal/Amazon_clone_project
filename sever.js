0e8cb1a1e3494f8d84a6119b5791e5d732e3f079
Vidhisha Agarwal <vidhishaagarwal262gmail.com>
2023-07-18 20:16:55 +0530

    updated

---
+++ sever.js
@@ -0,0 +1,10 @@
+const HTTP_PORT = process.env.PORT || 8080;
+const express=require("express");
+const app=express();
+const path = require("path");
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.listen(HTTP_PORT);
